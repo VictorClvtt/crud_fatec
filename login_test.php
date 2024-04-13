aea8b6fe6f86b00ee2e629b54c769773f666bf64
VictorClvtt <joaoclivatti@hotmail.com>
2024-04-13 09:34:55 -0300

    fifth commit

--- login_test.php
+++ login_test.php
@@ -1,36 +1,44 @@
 <?php
+    session_start();
     include_once("db_config.php");
 
-    $email = $_POST["email"];
-    $senha = $_POST["password"];
+    if(isset($_POST["email"]) && !empty($_POST["email"]) &&
+    isset($_POST["password"]) && !empty($_POST["password"])){
 
-    $stmt = $conn->prepare("SELECT * FROM usuario WHERE email = ?");
-    $stmt->bind_param("s", $email);
-    $stmt->execute();
-    $result = $stmt->get_result();
+        $email = $_POST["email"];
+        $senha = $_POST["password"];
     
-    if($result->num_rows == 1){
-        $user = $result->fetch_assoc();
-        if(password_verify($senha, $user['senha'])){
-            session_start();
-            $_SESSION["user_id"] = $user['idusuario'];
-
-            header("Location: sistema_tarefas.php");
-            exit;
+        $stmt = $conn->prepare("SELECT * FROM usuario WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if($result->num_rows == 1){
+            $user = $result->fetch_assoc();
+            if(password_verify($senha, $user['senha'])){
+                session_start();
+                $_SESSION["user_id"] = $user['idusuario'];
+    
+                header("Location: sistema_tarefas.php");
+                exit;
+            }else{
+                $_SESSION["msg"] = "Email ou senha inválidos!";
+    
+                header("Location: index.php");
+                exit;
+            }
         }else{
-            session_start();
-
             $_SESSION["msg"] = "Email ou senha inválidos!";
-
+    
             header("Location: index.php");
             exit;
         }
     }else{
-        session_start();
-
-        $_SESSION["msg"] = "Email ou senha inválidos!";
-
+        $_SESSION["msg"] = "Por favor, preencha todos os campos.";
+    
         header("Location: index.php");
         exit;
     }
+
+    
 ?>
